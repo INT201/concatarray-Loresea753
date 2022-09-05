5bee740cc4731f502bc85435ec85c129f53ce2a3
Loresea753 <woratan.2499@mail.kmutt.ac.th>
2022-09-05 14:42:55 +0700

    Finish

--- concatArray.js
+++ concatArray.js
@@ -7,14 +7,14 @@ function concatArray(array1, array2) {
   for(i=1;i<str.length;i++){
     if(str[i-1] === null||str[i-1] === undefined&&str[i] === null||str[i] === undefined){
       return undefined
-    }else if(str[i-1] === null||str[i-1] === undefined&&str[i] !== null&&str[i] !== undefined){
+    }else if(str[i-1] === null||str[i-1] === undefined&&(str[i] !== null&&str[i] !== undefined)){
       return array2
-    }else if(str[i-1] !== "")  {
+    }else if(str[i-1] !== "" && (str[str.length-1] !== null &&str[str.length-1] !== undefined))  {
       return str
     }else{return array1} }
     str = array1.concat(array2)
   return str
 }
 
-console.log(concatArray([5, 10, 15], [2, 4, 6, 7]))
+console.log(concatArray([2, 4, 6, 7], null))
 module.exports = concatArray
