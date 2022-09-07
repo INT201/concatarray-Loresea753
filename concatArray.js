ad75a28b8f282bc65d7245565816130ded7fac73
Loresea753 <woratan.2499@mail.kmutt.ac.th>
2022-09-07 19:36:33 +0700

    final_home

--- concatArray.js
+++ concatArray.js
@@ -4,17 +4,18 @@ function concatArray(array1, array2) {
   let str  = []
   str = str.concat(array1,array2)
   
-  for(i=1;i<str.length;i++){
-    if(str[i-1] === null||str[i-1] === undefined&&str[i] === null||str[i] === undefined){
-      return undefined
-    }else if(str[i-1] === null||str[i-1] === undefined&&(str[i] !== null&&str[i] !== undefined)){
-      return array2
-    }else if(str[i-1] !== "" && (str[str.length-1] !== null &&str[str.length-1] !== undefined))  {
-      return str
-    }else{return array1} }
-    str = array1.concat(array2)
-  return str
+  if(str[0]=== undefined&&str[str.length-1]===undefined){
+    return array1
+  }else if(str[0] === null||str[0] === undefined&&str[str.length-1] === null||str[str.length-1] === undefined){
+    return undefined
+  }else if((str[0] === null||str[0] === undefined)||(str[0] === "")&&(str[str.length-1] !== null&&str[str.length-1] !== undefined)){
+    return array2
+  }else if((str[0] !== null&&str[0] !== undefined)&&(str[str.length-1] === null||str[str.length-1] === undefined)||(str[str.length-1] === ""))  {
+    return array1
+  }else{
+    return str = array1.concat(array2)
+  }
 }
 
-console.log(concatArray([2, 4, 6, 7], null))
+console.log(concatArray([],[] ))
 module.exports = concatArray
